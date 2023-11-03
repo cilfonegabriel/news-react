386a64703b040d6711675d95ef4a2cb3a502d363
gabicilfone <cilfonegabriel@gmail.com>
2023-11-03 07:40:25 -0300

    Add form in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 
 
+import { Container, Grid, Typography } from "@mui/material"
+import Form from "./components/Form"
+
 function App() {
 
   return (
-    <h1>app noticias</h1>
-     
+    <Container>
+      <header>
+        <Typography align="center" marginY={5} component="h1" variant="h3">
+          News Search Engine
+        </Typography>
+      </header>
+
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} md={6} lg={4}>
+          <Form />
+        </Grid>
+      </Grid>
+    </Container>     
   )
 }
 
