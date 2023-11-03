fdf2298d84843f5fd5a70a25f59d20b5d20b37dc
gabicilfone <cilfonegabriel@gmail.com>
2023-11-03 08:14:38 -0300

    Chek news with the select category

--- src/context/NewsProvider.jsx
+++ src/context/NewsProvider.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState, useEffect, createContext } from "react";
 
 const NewsContext = createContext()
@@ -5,6 +6,18 @@ const NewsContext = createContext()
 const NewsProvider = ({children}) => {
 
     const [category, setCategory] = useState('general')
+    const [news, setNews] = useState([])
+
+    useEffect(() => {
+        const checkApi =async () => {
+            const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+
+            const { data } = await axios(url)
+
+            setNews(data.articles)
+        }
+        checkApi()
+    },[category])
 
     const handleChangeCategory = e => {
         setCategory(e.target.value);
